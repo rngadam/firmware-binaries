a4dc769f07a2507032bf949f36cca1dfff2a0710
Ricky Ng-Adam <rngadam@lophilo.com>
2012-07-20 14:00:34 +0800

    also need the path to nfsroot for this to work

--- nfsboot-debug.sh
+++ nfsboot-debug.sh
@@ -1,4 +1,4 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-NFSROOT=`cat nfsroot.txt`
+NFSROOT=`cat $DIR/nfsroot.txt`
 kexec -l $DIR/zImage-debug --append="mem=128M ip=dhcp noinitrd init=/sbin/init root=/dev/nfs nfsroot=$NFSROOT rw nfsrootdebug rootwait rootfstype=nfs"
 kexec -e
--- nfsboot.sh
+++ nfsboot.sh
@@ -1,5 +1,5 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-NFSROOT=`cat nfsroot.txt`
+NFSROOT=`cat $DIR/nfsroot.txt`
 kexec -l $DIR/zImage --append="mem=128M ip=dhcp noinitrd init=/sbin/init root=/dev/nfs nfsroot=$NFSROOT rw nfsrootdebug rootwait rootfstype=nfs"
 echo -n "current timestamp: " 
 date
