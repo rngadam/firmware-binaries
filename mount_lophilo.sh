4a375940a71af5ab4d0c8065b46ddbf14bb4cb7b
Ricky Ng-Adam <rngadam@lophilo.com>
2012-08-13 15:30:32 +0800

    useful indication of what is getting mounted

--- mount_lophilo.sh
+++ mount_lophilo.sh
@@ -2,5 +2,6 @@
 # assuming that the lophilo source directory
 # is at the same location than the Lophilo OS
 export NAME=`/media/BOOT/find_nfs.py`
+echo "Resolved as $NAME"
 mkdir -p ${HOME}/lophilo
 mount -t nfs -o cto ${NAME%.nfs} ${HOME}/lophilo
