e0ab8f5e986621959e89afff7ae68c47ccb6d7db
Ricky Ng-Adam <rngadam@lophilo.com>
2012-11-15 10:54:58 +0800

    new universal location for mounting lophilo dir

--- mount_lophilo.sh
+++ mount_lophilo.sh
@@ -2,6 +2,10 @@
 # assuming that the lophilo source directory
 # is at the same location than the Lophilo OS
 export NAME=`/media/BOOT/find_nfs.py`
+export MOUNT=/home/lophilo/lophilo
 echo "Resolved as $NAME"
-mkdir -p ${HOME}/lophilo
-mount -t nfs -o cto ${NAME%.nfs} ${HOME}/lophilo
+mkdir -p ${MOUNT}
+sudo mount -t nfs -o cto ${NAME%.nfs} ${MOUNT}
+if [ "$USER" != "lophilo" ]; then
+	su - lophilo
+fi
